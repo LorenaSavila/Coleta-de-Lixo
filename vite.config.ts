33fd090eba8f2b0af80ddd170f0c2c99dc8cbae6
weri wei <werenric@gmail.com>
2023-01-19 20:23:48 -0300

    'bosta'

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/coleta-de-lixo/",
+  base: "./coleta-de-lixo/",
   plugins: [react()],
 })
